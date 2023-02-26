7f5c0d3062ce259c1d70af6e6d97d2d53fda29e0
TrumanYen <yen.truman@gmail.com>
2023-02-25 17:59:16 -0700

    Product modal and product component aesthetics

--- Client/src/component/Product/Product.js
+++ Client/src/component/Product/Product.js
@@ -2,6 +2,7 @@ import React, { useEffect, useContext, useState } from 'react'
 import { DINOSGet, DINOSPost } from '../../scripts/backend-functions'
 import LoadingContext from "../../context/LoadingContext";
 import Input from "../Login/Input";
+import ProductModal from './ProductModal';
 
 
 
@@ -9,25 +10,37 @@ export default function Product({ name, description, price}){
 
     const { setLoading } = useContext(LoadingContext);
 
+    
+
     // possibly will add an array of tags
 
+    //product use state for testing
     const [product, setProduct] = useState({
         name: "Kendama",
         description: "Used it during all of grade 6, decided it was time to move on",
         price: 20,
         image: null,
     });
+    //use state for modal
+    const [showModal, setShowModal] = React.useState(false);
+    //function to load modal
+    const loadModal = () => {
+        console.log("loading modal",name, description, price );
+        setShowModal(true);
+    }
+
 
     return(
         <>
-        <div>
+        <div class = " rounded-lg bg-slate-100 hover:bg-red-200 hover:shadow-xl active:bg-red-400 m-4"
+             onClick={loadModal}>
             
-            <div class="max-w-sm rounded overflow-hidden shadow-lg">
+            <div class="max-w-sm rounded overflow-hidden shadow-lg ">
                 <img class="w-full" src="https://v1.tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains"/>
                 <div class="px-6 py-4">
                     <div class="font-bold text-xl mb-2">{name}</div>
                     <p class="text-gray-700 text-base">{description}</p>
-                    <p class="font-bold text-lg mb-2">{price} $</p>
+                    <p class="font-bold text-lg mb-2">${price}</p>
                 </div>
                 <div class="px-6 pt-1 pb-2">
                     <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
@@ -36,7 +49,12 @@ export default function Product({ name, description, price}){
                 </div>
             </div>
         </div>
-
+        {showModal ? (
+            <>
+                <ProductModal name = {name} description = {description} price = {price} setShowModal = {setShowModal} />
+            </>
+      ) : null}
+            
         </>
     )
 
---
+++ Client/src/component/Product/ProductModal.js
@@ -0,0 +1,90 @@
+import React, { useEffect, useContext, useState } from 'react'
+import { DINOSGet, DINOSPost } from '../../scripts/backend-functions'
+import LoadingContext from "../../context/LoadingContext";
+import img from '../../img/Gorilla.jpg';
+
+
+
+export default function ProductModal({ name, description, price, setShowModal}){
+    
+    const { setLoading } = useContext(LoadingContext);
+
+    // possibly will add an array of tags
+
+
+
+
+    return(
+        <>
+          <div
+            className="justify-center items-center mt-5 mb-5 overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative w-auto h-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl text-red-400 font-semibold mr-10">
+                    {name}
+                  </h3>
+                  <button
+                    className="text-black background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-0 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    X
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <img class="object-cover h-60 w-110 rounded shadow" src={img} alt="Sunset in the mountains"/>
+                  <div className="flex items-start justify-start p-1 rounded-t">
+                    <p className="my-2 text-slate-500 text-lg leading-relaxed">
+                        Seller 
+                    </p>
+                    <p class="inline-block bg-gray-400 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mx-2 my-2 ">
+                      Your mother
+                    </p>  
+                  </div>
+                  <div className="flex items-start justify-start p-1 rounded-t">
+                    <p className="my-2 text-slate-500 text-lg leading-relaxed">
+                      Price 
+                    </p>
+                    <p class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mx-2 my-2 ">
+                      ${price}
+                    </p>
+                  </div>
+                  <div className="flex items-start justify-start p-1 rounded-t">
+                    <p className="my-2 text-slate-500 text-lg leading-relaxed">
+                      Details 
+                    </p>
+                    <p class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mx-2 my-2 ">
+                      {description}
+                    </p>
+                  </div>
+                  
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <input type="text" placeholder="Write a message to seller" class="px-3 py-3 mr-7  placeholder-slate-400 text-slate-600 relative bg-slate-200 rounded text-sm border-0 shadow outline-slate-200 focus:outline-slate-300 focus:ring w-full">
+                  </input>
+                  <button
+                    className="bg-red-500 text-white active:bg-red-600 font-bold uppercase text-sm px-6 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => {
+                        setShowModal(false);
+                        console.log("Sending message, funct not implemented")
+                    }}
+                  >
+                    Message seller
+                  </button>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+    )
+
+}
