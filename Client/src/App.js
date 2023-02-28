39412a1432cf5d167f923e5511a9bf13c9076ff0
arrushigupta <arushi_gupta@icloud.com>
2023-02-28 16:49:18 -0700

    style update

--- Client/src/App.js
+++ Client/src/App.js
@@ -26,7 +26,7 @@ function App() {
 
   return (
 
-    <div className="App">
+    <div className="App ">
           <LoadingContext.Provider value={{ loading, setLoading }}>
             <UserContext.Provider value={{ userID, setUserID }}>
               <BrowserRouter>
