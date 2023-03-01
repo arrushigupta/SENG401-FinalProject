72cb4ff24c347f454202589f42f7fb13653c594f
TrumanYen <yen.truman@gmail.com>
2023-03-01 00:26:11 -0700

    More product aesthetics

--- Client/src/component/Product/ProductModal.js
+++ Client/src/component/Product/ProductModal.js
@@ -7,7 +7,7 @@ import { addMessageRoute } from "../../utils/Routes";
 import Button from '../Additional/Button'
 
 
-export default function ProductModal({ name, description, price, setShowModal}){
+export default function ProductModal({ name, description, price, category, setShowModal}){
     
     const { setLoading } = useContext(LoadingContext);
 
@@ -51,45 +51,51 @@ export default function ProductModal({ name, description, price, setShowModal}){
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
-                <div className="flex items-start bg-red-700 justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl text-neutral-50 ">
+                <div className="flex justify-start bg-red-700 p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="grow text-3xl text-neutral-50 ">
                     {name}
                   </h3>
+                  <span class="shrink inline-block h-fit self-end justify-self-start  ml-3 mr-20 bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 ">
+                    {category}
+                  </span>
                   <button
-                    className="text-black background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-0 mb-1 ease-linear transition-all duration-150"
-                    type="button"
+                    className=" shrink text-black self-start justify-self-end background-transparent font-bold uppercase  text-sm outline-none focus:outline-none mb-1 "
                     onClick={() => setShowModal(false)}
                   >
                     X
                   </button>
                 </div>
                 {/*body*/}
-                <div className="relative p-6 flex-auto bg-zinc-300">
-                  <img class="object-cover h-60 w-110 rounded shadow" src={img} alt="Sunset in the mountains"/>
-                  <div className="flex items-start justify-start p-1 rounded-t">
-                    <p className="my-2 text-slate-500 text-lg leading-relaxed">
-                        Seller 
-                    </p>
-                    <p class="inline-block bg-gray-400 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mx-2 my-2 ">
-                      Your mother
-                    </p>  
-                  </div>
-                  <div className="flex items-start justify-start p-1 rounded-t">
-                    <p className="my-2 text-slate-500 text-lg leading-relaxed">
-                      Price 
-                    </p>
-                    <p class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mx-2 my-2 ">
-                      ${price}
-                    </p>
-                  </div>
-                  <div className="flex items-start justify-start p-1 rounded-t">
-                    <p className="my-2 text-slate-500 text-lg leading-relaxed">
-                      Details 
-                    </p>
-                    <p class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mx-2 my-2 ">
-                      {description}
-                    </p>
-                  </div>
+                <div className="relative p-6 flex justify-center bg-zinc-300">
+                    <div className="w-fit">
+                        <img class="object-cover h-60 w-110 rounded shadow" src={img} alt="Sunset in the mountains"/>
+                        <div className="flex items-start justify-start p-1 rounded-t">
+                            <p className="my-2 text-slate-500 text-lg leading-relaxed">
+                                Seller 
+                            </p>
+                            <p class="inline-block bg-gray-400 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mx-2 my-2 ">
+                                Your mother
+                            </p>  
+                        </div>
+                        <div className="flex items-start justify-start p-1 rounded-t">
+                            <p className="my-2 text-slate-500 text-lg leading-relaxed">
+                                Price 
+                            </p>
+                            <p class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mx-2 my-2 ">
+                                ${price}
+                            </p>
+                        </div>
+                        <div className="flex items-start justify-start p-1 rounded-t">
+                            <p className="my-2 text-slate-500 text-lg leading-relaxed">
+                                Details 
+                            </p>
+                            <p class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mx-2 my-2 ">
+                                {description}
+                            </p>
+                        </div>                        
+
+                    </div>
+                  
                   
                 </div>
                 {/*footer*/}
