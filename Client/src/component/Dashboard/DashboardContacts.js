a33bd5ce24501f7d234f24f99ec19050f8ccf70d
zhifanl <zhifanli2000@gmail.com>
2023-03-01 11:14:20 -0700

    added contacts for dashboard

---
+++ Client/src/component/Dashboard/DashboardContacts.js
@@ -0,0 +1,176 @@
+import React,{useState,useEffect} from 'react'
+import styled from 'styled-components';
+// import Logo  from "../../assets/logo.svg"
+import Logo from "../../img/dinosM.png"
+function DashboardContacts({contacts,currentUser,changeChat}) {
+    const[currentUserName,setCurrentUserName] = useState(undefined);
+    const[currentUserImage,setCurrentUserImage] = useState(undefined);
+    const [currentSelected,setCurrentSelected] = useState(undefined);
+    useEffect(()=>
+    { 
+        if(currentUser){
+            setCurrentUserName(currentUser.username);
+            setCurrentUserImage(currentUser.avatarImage);
+            console.log(currentUser.username)
+            console.log(currentUser.avatarImage)
+
+            
+        }
+    },[currentUser]);
+
+    useEffect(()=>
+    { 
+        if(contacts){
+           console.log("received contacts", contacts)
+            
+        }
+    },[contacts]);
+
+
+    const changeCurrentChat = (index,contact)=>{
+        setCurrentSelected(index);
+        changeChat(contact);
+    };
+  return (
+    <>
+        {
+            currentUserName&&currentUserImage &&(
+
+            <Container>
+            <div className="brand">
+            <img src={Logo} alt="logo" />
+            <h3>Dinos Marketplace</h3>
+          </div>
+          <div className="contacts">
+            {contacts.map((contact, index) => {
+              return (
+                <div
+                  key={contact._id}
+                  className={`contact ${
+                    index === currentSelected ? "selected" : ""
+                  }`}
+                  onClick={() => changeCurrentChat(index, contact)}
+                >
+                  <div className="avatar">
+                    <img
+                      src={`data:image/svg+xml;base64,${contact.avatarImage}`}
+                      alt="avatar"
+                    />
+                  </div>
+                  <div className="username">
+                    <h3>{contact.username}</h3>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="current-user">
+            <div className="avatar">
+              <img
+                src={`data:image/svg+xml;base64,${currentUserImage}`}
+                alt="avatar"
+              />
+            </div>
+            <div className="username">
+              <h2>{currentUserName}</h2>
+            </div>
+          </div>
+            </Container>
+            )
+        }
+    </>
+  )
+}
+
+const Container = styled.div`
+ display: grid;
+  grid-template-rows: 10% 75% 15%;
+  overflow: hidden;
+  background-color: #F99417;
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    img {
+      height: 2rem;
+    }
+    h3 {
+      color: white;
+      text-transform: uppercase;
+    }
+  }
+  .contacts {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    overflow: auto;
+    gap: 0.8rem;
+    &::-webkit-scrollbar {
+      width: 0.2rem;
+      &-thumb {
+        background-color: #ffffff39;
+        width: 0.1rem;
+        border-radius: 1rem;
+      }
+    }
+    .contact {
+      background-color: #C21010;
+      min-height: 5rem;
+      cursor: pointer;
+      width: 90%;
+      border-radius: 0.2rem;
+      padding: 0.4rem;
+      display: flex;
+      gap: 1rem;
+      align-items: center;
+      transition: 0.5s ease-in-out;
+      .avatar {
+        img {
+          height: 3rem;
+        }
+      }
+      .username {
+        h3 {
+          color: white;
+        }
+      }
+    }
+    .selected {
+      background-color: #FBCB0A;
+    }
+  }
+  .current-user {
+    background-color: #FBCB0A;
+    border-radius: 0.2rem;
+    width: 90%;
+    margin-left: 1rem;
+    margin-bottom: 1rem;
+    padding: 0.4rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+    .avatar {
+      img {
+        height: 4rem;
+        max-inline-size: 100%;
+      }
+    }
+    .username {
+      h2 {
+        color: white;
+      }
+    }
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      gap: 0.5rem;
+      .username {
+        h2 {
+          font-size: 1rem;
+        }
+      }
+    }
+  }
+`;
+
+export default DashboardContacts
--- Client/src/pages/DashboardPage.js
+++ Client/src/pages/DashboardPage.js
@@ -1,21 +1,71 @@
 import Dashboard from "../component/Dashboard/Dashboard"
 import NavBar from "../component/NavBar/NavBar"
 import CreateProduct from "../component/Product/CreateProduct"
+import React, { useState, useEffect, useRef } from 'react'
+import { useNavigate } from 'react-router-dom';
+import DashboardContacts from '../component/Dashboard/DashboardContacts';
+import axios from 'axios';
+import { allUsersRoute } from '../utils/Routes'
 
 export default function DashboardPage() {
+
+    const [currentUser, setCurrentUser] = useState(undefined);
+    const [contacts, setContacts] = useState([]);
+    const [currentChat, setCurrentChat] = useState(undefined);
+    const navigate = useNavigate();
+
+
+    useEffect(() => {
+        async function fetchData() {
+          let userData = await JSON.parse(localStorage.getItem("chat-app-user"))
+          if (!userData) {
+            navigate("/");
+          }
+          else {
+            setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user"))); //user info, id, username, email
+            console.log(await JSON.parse(localStorage.getItem("chat-app-user")))
+          }
+        }
+        fetchData();
+      }, []);
     
+      useEffect(() => {
+        async function fetchData() {
+          if (currentUser) {
+            if (currentUser.isAvatarImageSet) {
+              const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+              console.log("User data", currentUser)
+              console.log("Contact data for all users", data)
+    
+              setContacts(data.data);
+            } else {
+              navigate('/setAvatar');
+            }
+          }
+        }
+        fetchData();
+      }, [currentUser]);
+
+      const handleChatChange = (chat) => {
+        setCurrentChat(chat)
+        console.log("current chat changed", chat)
+      }
+
+
     return (
         <>
             <NavBar />
+            <DashboardContacts contacts={contacts} currentUser={currentUser} changeChat={handleChatChange} />
+
             <div >
-            {/* <h1>My username is: {JSON.parse(localStorage.getItem("chat-app-user")).username}</h1>
+                {/* <h1>My username is: {JSON.parse(localStorage.getItem("chat-app-user")).username}</h1>
             <h1> Home </h1> */}
-            <Dashboard></Dashboard>
-            
-            {/* <ProductList />
+                <Dashboard></Dashboard>
+
+                {/* <ProductList />
             <CreateProduct/> */}
             </div>
-            
+
         </>
     )
 }
