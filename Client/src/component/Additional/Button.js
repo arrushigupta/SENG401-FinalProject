0736a366c5b81f8ffb550058e05a97017a0158dc
TrumanYen <yen.truman@gmail.com>
2023-03-01 17:53:33 -0700

    Removed dark mode settings
    (death to blue buttons)

--- Client/src/component/Additional/Button.js
+++ Client/src/component/Additional/Button.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 function Button(props) {
   return (
-    <button onClick={props.onClick} class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+    <button onClick={props.onClick} class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 ">
       {props.label}
     </button>
   );
 }
+//dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800
 
 export default Button;
