fc60a6b642331fea303c5e046800558434ae9c39
ernestnik26 <ernestnik26@gmail.com>
2023-03-21 21:16:13 -0600

    User input changes.

--- Client/src/component/Product/CreateProduct.js
+++ Client/src/component/Product/CreateProduct.js
@@ -10,7 +10,9 @@ import 'react-toastify/dist/ReactToastify.css';
 
 export default function CreateProduct() {
 
+
     const { setLoading } = useContext(LoadingContext);
+
 
     const [formValues, setFormValues] = useState({
         userID: JSON.parse(localStorage.getItem("chat-app-user"))._id,
@@ -23,8 +25,6 @@ export default function CreateProduct() {
 
     // Updates from values on input change
     const handleInputUpdate = (event) => {
-        console.log(event.target.name);
-
         setFormValues({
             ...formValues,
             [event.target.name]: event.target.value
@@ -33,10 +33,8 @@ export default function CreateProduct() {
 
     // Trying to connect backend to post product
     const handleSubmit = (event) => {
+        event.preventDefault();
 
-        console.log(formValues);
-
-        // console.log(product);
         DINOSPost("http://localhost:4000/api/postProduct", setLoading, {
             ...formValues, name: formValues.name,
             description: formValues.description, price: formValues.price, date: formValues.date, userID: formValues.userID
@@ -58,11 +56,23 @@ export default function CreateProduct() {
 
             <div className="grid-cols-3 gap-4 flex flex-col justify-center items-center ">
                 <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
-
-                    <input label="Name" name="name" required onChange={handleInputUpdate} class="bg-gray-50 border border-gray-500 text-gray-900 text-sm rounded-lg text-center" placeholder='Product Name' /><br />
-                    <input label="Price" name="price" required type='number' step='any' onChange={handleInputUpdate} class="bg-gray-50 border border-gray-500 text-gray-900 text-sm rounded-lg text-center" placeholder='Price' /><br />
-                    <input label="Category" name="category" onChange={handleInputUpdate} class="bg-gray-50 border border-gray-500 text-gray-900 text-sm rounded-lg text-center" placeholder='Category' /><br />
-                    <input label="Description" name="description" required onChange={handleInputUpdate} class="bg-gray-50 border border-gray-500 text-gray-900 text-sm rounded-lg text-center" placeholder='Description' /><br />
+                    <h1 className="text-2xl block mb-2 font-bold text-red-700"> Create New Product: </h1>
+                    <div className="mb-4">
+                        <label htmlFor="productName" className="block mb-2 font-bold text-gray-700">Product Name</label>
+                        <input type="text" name="name" id="productName" className="w-full px-3 py-2 placeholder-gray-400 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400" placeholder="Enter product name" value={formValues["name"]} onChange={handleInputUpdate} required />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="price" className="block mb-2 font-bold text-gray-700">Price</label>
+                        <input type="number" name="price" id="price" className="w-full px-3 py-2 placeholder-gray-400 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400" placeholder="Enter price" value={formValues["price"]} onChange={handleInputUpdate} required step='any'/>
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="category" className="block mb-2 font-bold text-gray-700">Category</label>
+                        <input type="text" name="category" id="category" className="w-full px-3 py-2 placeholder-gray-400 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400" placeholder="Enter category" value={formValues["category"]} onChange={handleInputUpdate} required />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="description" className="block mb-2 font-bold text-gray-700">Description</label>
+                        <textarea name="description" id="description" className="w-full px-3 py-2 placeholder-gray-400 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400" placeholder="Enter description" value={formValues["description"]} onChange={handleInputUpdate} required />
+                    </div>
                     <UploadImage />
 
                     <FormAction handleSubmit={handleSubmit} text="Submit" />
