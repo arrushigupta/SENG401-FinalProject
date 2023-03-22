5f3f7104ab9d250073e4397d3f63c557849c11c3
ernestnik26 <ernestnik26@gmail.com>
2023-03-21 18:24:45 -0600

    Navbar UI changes.

--- Client/src/component/NavBar/NavBar.js
+++ Client/src/component/NavBar/NavBar.js
@@ -1,7 +1,6 @@
 import img from '../../img/dinosM.png';
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import { useLocation } from 'react-router-dom';
-import UserContext from '../../context/UserContext';
 import { useNavigate } from "react-router-dom";
 import Button from '../Additional/Button'
 
@@ -14,11 +13,7 @@ export default function NavBar() {
 
     React.useEffect(() => {
         setActiveLink(location.pathname);
-    }, [location]);
-
-
-    const { userID, setUserID } = useContext(UserContext);
-    
+    }, [location]);    
 
     const handleClick = () => {
         // needs to delete sign in token.
@@ -27,14 +22,14 @@ export default function NavBar() {
       };
 
     return (
-        <nav id= "navbar" class="bg-white px-2 sm:px-4 py-2.5  fixed w-full z-20 top-0 left-0 border-b border-gray-200 ">
-            <div class="container flex flex-wrap items-center justify-between mx-auto">
-                <a href="/home" class="flex items-center">
-                    <img src={img} class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
-                    <span class="text-xl font-semibold whitespace-nowrap  text-neutral-50">Dinos Marketplace</span>
+        <nav id= "navbar" className="bg-white px-2 sm:px-4 py-2.5  fixed w-full z-20 top-0 left-0 border-b border-gray-200 flex justify-between items-center">
+            <div className="container flex flex-wrap items-center justify-between mx-auto">
+                <a href="/home" className="flex items-center">
+                    <img src={img} className="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
+                    <span className="text-xl font-semibold whitespace-nowrap  text-neutral-50">Dinos Marketplace</span>
                 </a>
-                <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-                    <ul class="flex flex-row p-4 mt-4 border rounded-2xl space-x-4 ">
+                <div className="flex items-center">
+                    <ul className="flex flex-row p-4 space-x-4 ">
                         <li>
                             <a href="/home" className={activeLink === '/home' ? 'active text-red-700 px-1 py-2 rounded-md text-md font-medium' : 'hover:text-red-700 px-1 py-2 rounded-md text-md font-medium text-white'}>Home</a>
                         </li>
@@ -47,11 +42,9 @@ export default function NavBar() {
                         <li>
                             <a href="/settings" className={activeLink === '/settings' ? 'active text-red-700 px-1 py-2 rounded-md text-md font-medium' : 'hover:text-red-700 px-1 py-2 rounded-md text-md font-medium text-white'}>Settings</a>
                         </li>
-
                     </ul>
-                </div>
-                <div class="flex md:order-2">
-                    <Button onClick={handleClick} label="Sign Out" />
+                    <Button onClick={handleClick} label="Sign Out"/>
+
                 </div>
             </div>
         </nav>
