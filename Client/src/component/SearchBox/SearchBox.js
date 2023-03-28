65f883db939c0196229935b54191699d926f29db
SamFarFar <sfarzamfar@yahoo.com>
2023-03-28 15:41:18 -0600

    test

--- Client/src/component/SearchBox/SearchBox.js
+++ Client/src/component/SearchBox/SearchBox.js
@@ -27,7 +27,7 @@ export default function SearchBox({
           type="text"
           value={searchQuery}
           onChange={handleSearch}
-          className="block w-full pl-4 mr-2 py-2 text-black-700 bg-white border rounded-full focus:border-red-400 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40"
+          className="block w-full  py-2 text-black-700 bg-white border rounded-full focus:border-red-400 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40"
           placeholder="Search Product..."
         />
 
