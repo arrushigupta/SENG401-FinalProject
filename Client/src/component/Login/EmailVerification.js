bfda29e21a17053486bc69608ba2551c4ee075ec
javier-capobianco <javier.capobianco.2210@gmail.com>
2023-03-28 15:40:51 -0600

    Revert "Merge branch 'main' of https://github.com/arrushigupta/SENG401-FinalProject"
    This reverts commit 888ef8311cb4bc795672fb0e6cb50743875daa4d, reversing
    changes made to 7db46d32627ba8bdd937b6192f290a9963f4b112.

--- Client/src/component/Login/EmailVerification.js
+++ Client/src/component/Login/EmailVerification.js
@@ -26,23 +26,28 @@ export default function EmailVerifcation({ user }) {
       <div className="flex items-center justify-center min-h-screen p-5  min-w-screen">
         <div className="">
           <img
-            className="w-16 md:w-32 lg:w-48 flex justify-start "
+            className="w-16 md:w-32 lg:w-48 flex justify-start mt-24 "
             src={img}
             alt="Image not loading"
           />
         </div>
-        <div className=" chat chat-start" style={{"margin-bottom": "15rem  ", "background-color": "#2a2d35"}}>
-          <div className="chat-bubble " style={{"background-color": "#2a2d35"}}>
-            <h3 className="text-2xl">
+        <div class="w-11  overflow-hidden inline-block">
+          <div class=" h-16  rotate-45 transform origin-bottom-left" style={{ "background-color": "#2a2d35" }}></div>
+        </div>
+        <div className="" style={{ "background-color": "#2a2d35", "padding": "0.5rem 0.75rem 0.75rem 0.75rem", "border-radius": "25px" }}>
+          <div className=" " >
+            <h3 className="text-2xl text-white">
               An email has been sent to your email address.
             </h3>
             <br />
-            <h3 className="text-xl">
+            <h3 className="text-xl text-white">
               Please check your mailbox to verify email!
             </h3>
             <br />
-            <h3 className="text-xl">
-              Note that ucalgary email verification may take up to two hours.
+            <h3 className="text-xl text-white">
+              Note that ucalgary email verification may take up to two hours.</h3>
+            <br />
+            <h3 className="text-xl text-white"> Usually the ucalgary address will quarantine this message, please click <a className="text-red-600" href="https://security.microsoft.com/quarantine">here</a> and log in to release the message.
             </h3>
           </div>
         </div>
@@ -53,16 +58,14 @@ export default function EmailVerifcation({ user }) {
       <div className="flex items-center justify-center min-h-screen p-5  min-w-screen">
         <div className="">
           <img
-            className="w-16 md:w-32 lg:w-48 flex justify-start "
+            className="w-16 md:w-32 lg:w-48 flex justify-start mt-24 "
             src={img}
             alt="Image not loading"
           />
         </div>
-        <div className=" chat chat-start" style={{"margin-bottom": "15rem  ", "background-color": "#2a2d35"}}>
-          <div className="chat-bubble " style={{"background-color": "#2a2d35"}}>
-            <h3 className="text-2xl">
-              Verifying email...
-            </h3>
+       <div className="w-1/2 h-1/2" style={{ "background-color": "#2a2d35", "padding": "0.5rem 0.75rem 0.75rem 0.75rem", "border-radius": "25px" }}>
+          <div className=" " >
+            <h3 className="text-xl text-white">Verifying email...</h3>
           </div>
         </div>
       </div>
