bec626b56c312c71a3f85b48b0c0f8a2ce9c115f
666dragonz <tanishdatta@hotmail.com>
2023-03-28 13:35:28 -0600

    Email verify page done

--- Client/src/component/Login/EmailVerification.js
+++ Client/src/component/Login/EmailVerification.js
@@ -4,6 +4,8 @@ import { useEffect, useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { DINOSPatch } from "../../scripts/backend-functions";
 import { verifyEmailRoute } from "../../utils/Routes";
+import img from "../../img/dinosM.png";
+
 
 export default function EmailVerifcation({ user }) {
   const navigate = useNavigate();
@@ -23,15 +25,28 @@ export default function EmailVerifcation({ user }) {
 
   if (user === "?") {
     return (
-      <div>
-        <h1>Please Verify Email</h1>
+
+      <div class="flex items-center justify-center min-h-screen p-5  min-w-screen">  
+        <div div class="max-w-xl p-8 text-center text-gray-800 bg-white shadow-xl lg:max-w-3xl rounded-3xl lg:p-12">
+          
+          <img class="w-16 md:w-32 lg:w-48 flex justify-start" src={img} alt="Image not loading"/> 
+
+          <h3 class="text-2xl">Please Verify Email</h3>
+        </div>
       </div>
     );
   } else {
     return (
-      <div>
-        <h1>Verifying Email...</h1>
+
+      <div class="flex items-center justify-center min-h-screen p-5  min-w-screen">  
+        <div div class="max-w-xl p-8 text-center text-gray-800 bg-white shadow-xl lg:max-w-3xl rounded-3xl lg:p-12">
+          
+          <img class="w-16 md:w-32 lg:w-48 flex justify-start" src={img} alt="Image not loading"/> 
+
+          <h3 class="text-2xl">Verifying Email...</h3>
+        </div>        
       </div>
+
     );
   }
 }
