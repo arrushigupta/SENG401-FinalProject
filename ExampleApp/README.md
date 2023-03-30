2a0bdbc9c25fb3718dabdb7308ff2244d3b14901
zhifanl <zhifanli2000@gmail.com>
2023-03-29 22:35:50 -0600

    readme.md

--- ExampleApp/README.md
+++
@@ -1,1 +0,0 @@
-# cpsc471-project
--- ExampleApp/client/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Footer() {
-    return (
-        <footer className="fixed inset-x-0 bottom-0">
-            <small>2022 © AIR Medical</small>
-        </footer>
-    )
-}
